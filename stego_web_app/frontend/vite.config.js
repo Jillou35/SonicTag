fb97b72939aee6288abcf4cb71d756e902f7b5a4
Jillian Strenk <jillian.strenk@posteo.net>
2026-02-01 13:04:11 +0100

    change config

--- stego_web_app/frontend/vite.config.js
+++ stego_web_app/frontend/vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: true, // Needed for Docker
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'http://web:8000',
-                changeOrigin: true,
-            }
-        }
+        port: 5173
     }
 })
